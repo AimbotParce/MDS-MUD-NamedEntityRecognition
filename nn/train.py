5987df34c137299a0a5583e29593f76844453aa8
AimbotParce <marcparcerisa@gmail.com>
2025-04-20 23:41:17 +0200

    Fuck off

--- nn/train.py
+++ nn/train.py
@@ -3,50 +3,44 @@
 import sys
 from contextlib import redirect_stdout
 
-from tensorflow.keras import Input
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional, concatenate, Lambda
-
-from dataset import *
 from codemaps import *
-
-def build_network(codes) :
-
-   # sizes
-   n_words = codes.get_n_words()
-   n_sufs = codes.get_n_sufs()
-   n_labels = codes.get_n_labels()   
-   max_len = codes.maxlen
-
-   inptW = Input(shape=(max_len,)) # word input layer & embeddings
-   embW = Embedding(input_dim=n_words, output_dim=100,
-                    input_length=max_len, mask_zero=True)(inptW)  
-   
-   inptS = Input(shape=(max_len,))  # suf input layer & embeddings
-   embS = Embedding(input_dim=n_sufs, output_dim=50,
-                    input_length=max_len, mask_zero=True)(inptS) 
-
-   dropW = Dropout(0.1)(embW)
-   dropS = Dropout(0.1)(embS)
-   drops = concatenate([dropW, dropS])
-
-   # biLSTM   
-   bilstm = Bidirectional(LSTM(units=200, return_sequences=True,
-                               recurrent_dropout=0.1))(drops) 
-   # output softmax layer
-   out = TimeDistributed(Dense(n_labels, activation="softmax"))(bilstm)
-
-   # build and compile model
-   model = Model([inptW,inptS], out)
-   model.compile(optimizer="adam",
-                 loss="sparse_categorical_crossentropy",
-                 metrics=["accuracy"])
-   
-   return model
-   
+from dataset import *
+from keras import Input
+from keras.layers import LSTM, Bidirectional, Dense, Dropout, Embedding, Lambda, TimeDistributed, concatenate
+from keras.models import Model
 
 
-## --------- MAIN PROGRAM ----------- 
+def build_network(codes):
+
+    # sizes
+    n_words = codes.get_n_words()
+    n_sufs = codes.get_n_sufs()
+    n_labels = codes.get_n_labels()
+    max_len = codes.maxlen
+
+    inptW = Input(shape=(max_len,))  # word input layer & embeddings
+    embW = Embedding(input_dim=n_words, output_dim=100, input_length=max_len, mask_zero=True)(inptW)
+
+    inptS = Input(shape=(max_len,))  # suf input layer & embeddings
+    embS = Embedding(input_dim=n_sufs, output_dim=50, input_length=max_len, mask_zero=True)(inptS)
+
+    dropW = Dropout(0.1)(embW)
+    dropS = Dropout(0.1)(embS)
+    drops = concatenate([dropW, dropS])
+
+    # biLSTM
+    bilstm = Bidirectional(LSTM(units=200, return_sequences=True, recurrent_dropout=0.1))(drops)
+    # output softmax layer
+    out = TimeDistributed(Dense(n_labels, activation="softmax"))(bilstm)
+
+    # build and compile model
+    model = Model([inptW, inptS], out)
+    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+    return model
+
+
+## --------- MAIN PROGRAM -----------
 ## --
 ## -- Usage:  train.py ../data/Train ../data/Devel  modelname
 ## --
@@ -63,12 +57,12 @@ valdata = Dataset(validationdir)
 # create indexes from training data
 max_len = 150
 suf_len = 5
-codes  = Codemaps(traindata, max_len, suf_len)
+codes = Codemaps(traindata, max_len, suf_len)
 
 # build network
 model = build_network(codes)
-with redirect_stdout(sys.stderr) :
-   model.summary()
+with redirect_stdout(sys.stderr):
+    model.summary()
 
 # encode datasets
 Xt = codes.encode_words(traindata)
@@ -77,10 +71,9 @@ Xv = codes.encode_words(valdata)
 Yv = codes.encode_labels(valdata)
 
 # train model
-with redirect_stdout(sys.stderr) :
-   model.fit(Xt, Yt, batch_size=32, epochs=10, validation_data=(Xv,Yv), verbose=1)
+with redirect_stdout(sys.stderr):
+    model.fit(Xt, Yt, batch_size=32, epochs=10, validation_data=(Xv, Yv), verbose=1)
 
 # save model and indexs
 model.save(modelname)
 codes.save(modelname)
-
---
+++ run-nn.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+set -e
+set -u
+set -o pipefail
+
+# train NN
+echo "Training NN"
+python3 nn/train.py data/train data/devel models/mymodel
+
+# run model on devel data and compute performance
+echo "Predicting and evaluatig"
+python3 nn/predict.py models/mymodel data/devel data/devel.out | tee data/devel.stats
