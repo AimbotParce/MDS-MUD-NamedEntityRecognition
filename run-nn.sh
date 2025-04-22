506c95e0dce06a1cebb30e0dedf3389bbb81de8b
AimbotParce <marcparcerisa@gmail.com>
2025-04-22 17:27:42 +0200

    Mkdir models

--- run-nn.sh
+++ run-nn.sh
@@ -3,10 +3,14 @@ set -e
 set -u
 set -o pipefail
 
+if [ ! -d models ]; then
+    mkdir models
+fi
+
 # train NN
 echo "Training NN"
-python3 nn/train.py data/train data/devel models/mymodel
+python3 nn/train.py data/train data/devel models/mymodel.h5
 
 # run model on devel data and compute performance
 echo "Predicting and evaluatig"
-python3 nn/predict.py models/mymodel data/devel data/devel-nn.out | tee data/devel-nn.stats
+python3 nn/predict.py models/mymodel.h5 data/devel data/devel-nn.out | tee data/devel-nn.stats
