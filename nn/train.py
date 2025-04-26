fc10c71bf5e78265eb5ee6eba11b5ef61914706b
AimbotParce <marcparcerisa@gmail.com>
2025-04-27 00:45:13 +0200

    Remove class weights

--- nn/train.py
+++ nn/train.py
@@ -8,7 +8,8 @@ import tensorflow as tf
 from codemaps import *
 from dataset import *
 from keras import Input
-from keras.layers import Conv1D, Dense, Dropout, Embedding, Reshape, TimeDistributed, concatenate
+from keras.layers import (Conv1D, Dense, Dropout, Embedding, Reshape,
+                          TimeDistributed, concatenate)
 from keras.models import Model
 
 
@@ -136,13 +137,7 @@ if __name__ == "__main__":
     with redirect_stdout(sys.stderr):
         model.summary()
 
-    class_weights = np.zeros(codes.get_n_labels(), dtype=np.float32)
-    for label, index in codes.label_index.items():
-        if label not in ["PAD", "UNK"]:
-            class_weights[index] = 1 / np.sqrt(codes.class_counts[label])
-
-    class_weights[codes.label_index["UNK"]] = 0.0
-    class_weights = class_weights / np.sum(class_weights)
+    class_weights = np.ones(codes.get_n_labels(), dtype=np.float32)
 
     print(f"{'Class':<10s}  {'Count':<10s}  Weight", file=sys.stderr)
     for label, index in codes.label_index.items():
